59e42f642cc32612e1c70a32ee60e1151f664df1
unknown <dusanthansivarajah@hotmail.com>
2024-03-28 20:22:20 +0200

    added all the funtions, all funtions should be working , theres still email and invoice as well as creating a GUI which is up next

--- POS.py
+++ POS.py
@@ -17,47 +17,96 @@
 
 
 #fucntions for Admin
-def addToStock(adminStock,productName, productQuantity, productPrice):# admin adds products to the stock
-    productQuantity = int(input(f"Enter the quantity added to the Stock for {productName}: "))
-    productPrice = float(input(f"Enter the Price for {productName}:  "))
+def addToStock(adminStock,productName):# admin adds products to the stock
+  productQuantity = int(input(f"Enter the quantity added to the Stock for {productName}: "))
+  productPrice = float(input(f"Enter the Price for {productName}:  "))
+  if productName in adminStock and productQuantity==0:
+      del adminStock[productName]
+  elif productName in adminStock:
+    originalQuantitiy = adminStock[productName][0]
+    adminStock[productName]=[productQuantity+originalQuantitiy,productPrice]
+    print(f"the Quantity of {productName} has been updated")
+  else:
     adminStock[productName]=[productQuantity,productPrice]
+    print(f"{productName} has been added to the stock, with Quantity of {productQuantity} and Price {productPrice}")
+  return adminStock
 
-def removeFromStock(adminStock,productName):
+def removeFromStock(adminStock,productName): # might not need this 
     del adminStock[productName]
 
-def sortByProduct(adminStock):
+def sortByProduct(adminStock): # sorts products in alphabetical order 
     adminStock = dict(sorted(adminStock.items())) 
     for items, info in adminStock.items():
-        print(f"Product Name: {items}        Quantity: {info[0]}        Price per item: ${info[1]}" )
+        print(f"Product Name: {items}        Quantity: {info[0]}        Price per item: ${info[1]:.2f}" )
     return adminStock
 
-#Functions for Costomer
-def addToCart(stock, cart, productName,productQuantity, productPrice, buyQuantity, itemQuantity): # adds item to cart and updates the stock
-    if productName in cart:# if the product is already in the cart, only add the quantity to the cart and update the stock
-        cart[productName] = [itemQuantity+buyQuantity,productPrice]
-        stock[productName] = [productQuantity-buyQuantity, productPrice]
-    else:
-        cart[productName] = [buyQuantity, productPrice]
-        stock[productName] = [productQuantity-buyQuantity, productPrice]
+def sortByQuantity(adminStock): # converts to a list then back to a dictionary and sorts Quantity by decending order 
+    adminStock= dict(sorted(adminStock.items(), key=lambda x: x[1], reverse=True))
+    for items, info in adminStock.items():
+        print(f"Product Name: {items}        Quantity: {info[0]}        Price per item: ${info[1]:.2f}" )
     
-def removeFromCart(stock, cart, productName,productQuantity, productPrice, removeQuantity, buyQuantity):# removes products from the customer cart and sends it back to the stock
-    stock[productName] = [productQuantity+removeQuantity, productPrice]
-    cart[productName] = [buyQuantity-removeQuantity, productPrice]
-
-def sortByQuantity(cart): #sorts Quantity by decending order 
-    cart= sorted(cart.items(), key=lambda x: x[1][0], reverse=True)
+#Functions for Costomer
+def addToCart(marketStock, productName, cart): # adds item to cart and updates the stock
+    itemPrice =  marketStock[productName][1]
+    stockQuantity = marketStock[productName][0]
+    buyQuantity = int(input(f"Enter the quantity of {productName}'s you would like to purchase: "))
+    if buyQuantity > stockQuantity: # if the customer wants to buy more than the quantity in the stock available , he has a choice to buy the remaining products
+        choice=input(f" Unfortunatly we dont have that much supplies for {productName}, there is only {stockQuantity}, would you like to purchase all of the remaining product?(type 'yes' to make a purchase)")
+        if choice=="yes":
+            cart[productName] = [stockQuantity, itemPrice]
+            marketStock[productName] = [0, itemPrice]
+        else:
+            print("\n Thats alright, theres plenty of other items you can still buy")
+    elif productName in cart:# if the product is already in the cart, only add the quantity to the cart and update the stock
+        quantityInCart = cart[productName][0]
+        cart[productName] = [buyQuantity+quantityInCart, itemPrice]
+    else:
+        cart[productName] = [buyQuantity, itemPrice]
+    marketStock[productName] = [stockQuantity-buyQuantity, itemPrice]
+    return cart
+    
+def removeFromCart(marketStock, cart):# removes products from the customer cart and sends it back to the stock
+    productName = input("Which product would you like to remove from the cart? ")  
+    
+    ## check if issues , i think the issues is if im romoving from the cart , its not adding back to the stock
+    
+    if productName in cart:
+        itemPrice =  cart[productName][1]
+        
+        quantityInCart = cart[productName][0]
+        
+        removeQuantity =  int(input(f"How much quantity of {productName} would you like to put back on the shelf? "))
+        if removeQuantity>quantityInCart:
+            returnAll=input(f"Would you like to return all {quantityInCart} of {productName}? (type yes)")# returning all qantities in the cart 
+            if returnAll == "yes" and productName  not in marketStock:
+                marketStock[productName] = [quantityInCart,itemPrice]
+                cart[productName] = [0,itemPrice]
+            else:# removing all items in the cart and adding it to the quantitiy in the stock 
+                stockQuantity = marketStock[productName][0]
+                marketStock[productName] = [stockQuantity+quantityInCart,itemPrice]
+                cart[productName] = [0,itemPrice]
+          
+         
+        else:
+            stockQuantity = marketStock[productName][0]
+            marketStock[productName] = [stockQuantity+removeQuantity,itemPrice]
+            cart[productName] = [quantityInCart-removeQuantity,itemPrice]
 
 def recipt(cart, totalSum):
     for items, info in cart.items():
-        print(f"Product Name:{items}------ Quantity:{info[0]}------Price per item:{info[1]}------Product Sum{info[0]*info[1]}" )
+        print(f"\nProduct Name:{items}------ Quantity:{info[0]}------Price per item:{info[1]}------Product Sum{info[0]*info[1]}" )
         totalSum+= info[0]*info[1]
     print(f"your total is {totalSum:.2f}")
 
-def checkQuantity(compartment): # this is to check the quantity of either stock or cart
+def checkQuantity(compartment): # this is to check the quantity of either stock or cart is =0 , if yes , it removes the item 
     storageCopy =  compartment.copy() # creating a copy of the dictionary beacuse we cant delete items while iterating over the orginal dictionary
     for item,info in storageCopy.items():
         if info[0]<=0:
             del compartment[item]
+    for items, info in compartment.items():
+        print(f"Product Name: {items}        Quantity: {info[0]}        Price per item: ${info[1]:.2f}" )
+    return compartment
+    
 def sendEmail():
     pass
 def downloadInvoice():
@@ -71,19 +120,19 @@ stock = {"Apples":[100,1.3],
          "books":[10,4.3],
          "lights":[18,10.50],
          "chocolates":[122,1.3],
-         "pigs":[50,270],
+         "rat":[50,270],
          "apples":[100,1.3], 
          "Bananas":[737,2.30],
          "bbq":[15,2.13] }
-print(stock)
+checkQuantity(stock)
 
-who_Am_I = int(input("Hello and Welcome! Who are you?(1,2,3)\n 1.Admin \n 2.Customer \n 3.Exit Program \n Awating for your Response: "))
+who_Am_I = int(input("Hello and Welcome! Who are you?(1,2,3)\n 1.Admin \n 2.Customer \n 3.Exit Program \n Awaiting for your Response: "))
 if who_Am_I==1: #Admin Control 
     print("Admin Contorl")
     adminSelection = None
     adminUsername = input("Enter Username:")
     adminPassword = input("Enter Password:")
-    if adminUsername == "Admin" and adminPassword == "Admin123": #Acess granted to admin contorl 
+    if adminUsername == "Admin" and adminPassword == "Admin123": #Dummy authentication, Acess granted to admin contorl 
     
         stock= sortByProduct(stock)  # look for a better sorting method that allows the  user to take big data.
         # print(stock)
@@ -96,21 +145,24 @@ if who_Am_I==1: #Admin Control
         
         
         
-        adminSelection= input("select your choice")
+        adminSelection= int(input("select your choice"))
         if adminSelection == 1:
             product = input("Which item would you like to modify?")
             if product in stock:
-                pass #modify the sock ******, if quantity = 0 remove the product from the stock 
+                stock = addToStock(stock,product) #modify the sock ******, if quantity = 0 remove the product from the stock
+                stock = checkQuantity(stock) 
+
             else:
-                addProduct= input("would you like to add this product to the stock?\n 1.Yes \n 2.No \n Awaiting your choice ")
+                addProduct= int(input("would you like to add this product to the stock?\n 1.Yes \n 2.No \n Awaiting your choice "))
                 if addProduct == 1:
-                    pass # append product to the stock with quantity and price
+                    stock= addToStock(stock,product) # append product to the stock with quantity and price
+                    stock = checkQuantity(stock)
                 elif addProduct == 2:
                     pass # admin is taken back to admin selection menu 
                 else:
                     print("please select appropriate choice")# take admin back to addproduct
         elif adminSelection==2:
-            pass # sort the stock dictionary by quantity 
+            sortByQuantity(stock) # sort the stock dictionary by quantity 
         else:
             print("please select the appropriate choice")# take the admin back to the admin selection menu
 
@@ -124,24 +176,42 @@ if who_Am_I==1: #Admin Control
 
 elif who_Am_I==2:#Customer 
     customerCart = {}
+    buyProduct = None
     print("Hello dear Customer, have fun shopping")
-    while buyProduct != -1: # or quantity not = -1  , repeatedly ask the cutomer to by products
-        print(stock)
-        buyProduct= input("Which product would you like to purchase dear?")
-        if buyProduct in stock:
-            pass # append the item in customerCart and add quantity , subtract the quantitiy from the stock, 
-                 #if quantity > stock quantity , then display a message if the customer wants to buy the remaining stock ,
-                 # if yes , set the customer quantity to the remaining quantity in the stock then remove the item from the stock
-                 # if no then send the customer back to buyProduct   
-        else:
-            print("this product does not exit , please check the stock to see all available products") #send the cutomer back to buyProdcuts 
+    print ("    ---------------------Market items------------------- \n")
+    checkQuantity(stock)
+    while buyProduct != "-1": # or quantity not = -1  , repeatedly ask the cutomer to by products
+        customerSelect= int(input("What would you like to do?\n 1. buy a product ?\n 2. remove product from cart?\n 3. Checkout\n Awaiting for your response: "))
+        if customerSelect==1:
+            buyProduct= input("\nWhich product would you like to purchase dear? ")
+            if buyProduct in stock:
+                customerCart= addToCart(stock,buyProduct,customerCart) # append the item in customerCart and add quantity , subtract the quantitiy from the stock, 
+                print ("    ---------------------Market items------------------- \n")
+                stock = checkQuantity(stock)
+                print ("    ---------------------Items in Cart------------------- \n")
+                customerCart = checkQuantity(customerCart)
+                
+                    #if quantity > stock quantity , then display a message if the customer wants to buy the remaining stock ,
+                    # if yes , set the customer quantity to the remaining quantity in the stock then remove the item from the stock
+                    # if no then send the customer back to buyProduct   
+            else:
+                print("This product does not exit , please check the stock to see all available products") #send the cutomer back to buyProdcuts 
+        if customerSelect==2:
+            removeFromCart(stock,customerCart)
+            print ("    ---------------------Market items------------------- \n")
+            stock = checkQuantity(stock)
+       
+            print ("    ---------------------Items in Cart------------------- \n")
+            customerCart= checkQuantity(customerCart)
+        if customerSelect==3:
+            invoice= int(input("Would you like a invoice?\n 1. by email\n 2. Download invoice\n 3. No "))# create a message that showes the order details and total price 
+            if invoice == 1:
+                pass # import smtp to send email by gmail
+            elif invoice==2:
+                pass # save invoice as a txt document in local repository, give directory location. 
 
     print(customerCart)
-    invoice= int(input("would you like a invoice?\n 1. by email\n 2. Download invoice"))# create a message that showes the order details and total price 
-    if invoice == 1:
-        pass # import smtp to send email by gmail
-    elif invoice==2:
-        pass # save invoice as a txt document in local repository, give directory location. 
+    
 
 
 
